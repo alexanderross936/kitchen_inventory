bcbdb7cf1c607ba7cf9b6cdcfaaf4c4459513409
Alexander Ross <alexanderross936@gmail.com>
2020-08-10 19:09:15 -0500

    change

---
+++ frontend/build/precache-manifest.024b0679f6d8f4ed7fd4fb5f4e4bf0d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b420332ee0511eb6fb5bf8901182c1e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b620190ff6b92b4e014",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b3c8ecf985784bef0e6a",
+    "url": "/static/js/2.90a9d948.chunk.js"
+  },
+  {
+    "revision": "982d21e92646d4f5066b07e449df3ad5",
+    "url": "/static/js/2.90a9d948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b620190ff6b92b4e014",
+    "url": "/static/js/main.9e97ff25.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
