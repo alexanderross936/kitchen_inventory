b8419728a7184776d376bcf3789e379cdf85fa34
Alexander Ross <alexanderross936@gmail.com>
2020-08-10 20:05:10 -0500

    change

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.static('frontend/public'));
+app.use(express.static('frontend/build'));
 // app.use(function (req, res, next) {
 //     res.setHeader('Access-Control-Allow-Origin', '*');
 //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
