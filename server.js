fc9187a411a47a1904b95314bdefc39328a0e803
Alexander Ross <alexanderross936@gmail.com>
2020-08-12 14:09:44 -0500

    deploy

--- server.js
+++ server.js
@@ -324,6 +324,14 @@ app.post('/add_recipe', auth, async(req, res) => {
 
 })
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('frontend/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
+
 app.listen(4000, () => {
     console.log('App listening to you')
 })
